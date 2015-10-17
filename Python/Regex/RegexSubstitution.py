9d5c9050a06d350d46bb77f64777b139a8907af3
bk2dcradle <ankitsultana@gmail.com>
2015-10-17 23:46:26 +0530

    Bug Fix

--- Python/Regex/RegexSubstitution.py
+++ Python/Regex/RegexSubstitution.py
@@ -1,9 +1,8 @@
 import re
-
 for tc in xrange(int(raw_input())):
     line = str(raw_input())
     pattern = re.compile(r'(?<= )&&(?= )')
-    res = re.sub(pattern, "and", line)
-    pattern = re.compile('(?<= )\|\|(?= )')
-    res = re.sub(pattern, "or", res)
-    print res
+    line = re.sub(pattern, "and", line)
+    pattern = re.compile(r'(?<= )\|\|(?= )')
+    line = re.sub(pattern, "or", line)
+    print line
